921922f059838b69c55b4dae83880e2571c98014
mahmoudElsawah123 <mahmoudelsawah21@gmail.com>
2023-07-25 15:03:08 +0300

    hello

--- app/page.js
+++ app/page.js
@@ -6,20 +6,35 @@ const HomeCarousel = dynamic(() => import('./components/HomeCarousel/HomeCarouse
   ssr : false,
   loading : ()=> <Loading/>
 });
-const AboutUs = dynamic(() => import('./components/home-page/AboutUs'));
-const Goals = dynamic(() => import('./components/Goals/Goals'));
-// const Category = dynamic(() => import('./components/Category/Category'));
-// import {getArticleData, getCategoryData } from './lib/DataFetching';
-// const LatestNews = dynamic(() => import('./components/latestNews/LatestNews'));
-const InstagramBanner = dynamic(() => import('./components/InstagramBanner/InstagramBanner'));
+const AboutUs = dynamic(() => import('./components/home-page/AboutUs'),{
+  ssr : false,
+  loading : ()=> <Loading/>
+});
+const Goals = dynamic(() => import('./components/Goals/Goals'),{
+  ssr : false,
+  loading : ()=> <Loading/>
+});
+const Category = dynamic(() => import('./components/Category/Category'),{
+  ssr : false,
+  loading : ()=> <Loading/>
+});
+import {getArticleData, getCategoryData } from './lib/DataFetching';
+const LatestNews = dynamic(() => import('./components/latestNews/LatestNews'),{
+  ssr : false,
+  loading : ()=> <Loading/>
+});
+const InstagramBanner = dynamic(() => import('./components/InstagramBanner/InstagramBanner'),{
+  ssr : false,
+  loading : ()=> <Loading/>
+});
 const Distinguishes = dynamic(() => import('./components/Distinguishes/Distinguishes'),{
   ssr : false,
   loading : ()=> <Loading/>
 });
 
 export default async function Home () {
-  // const data = await getArticleData();
-  // const CategoryData = await getCategoryData();
+  const data = await getArticleData();
+  const CategoryData = await getCategoryData();
   return (
     <>
         <HomeCarousel/>
@@ -29,15 +44,15 @@ export default async function Home () {
         <section>
             <Goals/>
         </section>
-        {/* <section>
+        <section>
         <Category CategoryData={CategoryData.data}/>
-        </section> */}
+        </section>
         <section>
         <Distinguishes/>
         </section>
-        {/* <section>
+        <section>
             <LatestNews data={data}/>
-           </section> */}
+           </section>
             <section className='instagram-banner'>
               <InstagramBanner/>
             </section>
